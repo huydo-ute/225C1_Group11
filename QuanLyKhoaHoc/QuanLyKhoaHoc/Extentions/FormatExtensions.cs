c9769d9749aabf0d6ef45ee05a5e635fe410d8db
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-05-30 09:27:40 +0700

    QuachNhatPhuong_Create_Extentions

---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Extentions/FormatExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace QuanLyKhoaHoc.Extensions
+{
+    public static class FormatExtensions
+    {
+        public static string ToHocPhiVN(this decimal hocPhi)
+        {
+            return hocPhi.ToString("N0", new CultureInfo("vi-VN")) + " VNĐ";
+        }
+
+        public static string ToNgayVN(this DateTime ngay)
+        {
+            return ngay.ToString("dd/MM/yyyy");
+        }
+
+    }
+}
