92c81d490c5d909c8e1b138494887d645e7c840d
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-05-30 09:09:57 +0700

    QuachNhatPhuong_Create_Model

---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Models/GiangVien.cs
@@ -0,0 +1,16 @@
+﻿namespace QuanLyKhoaHoc.Models
+{
+    public class GiangVien
+    {
+        public int Id { get; set; }
+
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+
+        public string ChuyenMon { get; set; }
+        public int SoNamKinhNghiem { get; set; }
+
+        public ICollection<KhoaHoc> KhoaHocs { get; set; }
+    }
+}
---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Models/KhoaHoc.cs
@@ -0,0 +1,20 @@
+﻿using QuanLyKhoaHoc.Enums
+namespace QuanLyKhoaHoc.Models
+{
+    public class KhoaHoc
+    {
+        public int Id { get; set; }
+
+        public string TenKhoaHoc { get; set; }
+        public string MoTa { get; set; }
+        public TrangThaiKhoaHoc TrangThai { get; set; }
+        public decimal HocPhi { get; set; }
+        public int SoBuoiHoc { get; set; }
+
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+
+        public int GiangVienId { get; set; }
+        public GiangVien GiangVien { get; set; }
+    }
+}
