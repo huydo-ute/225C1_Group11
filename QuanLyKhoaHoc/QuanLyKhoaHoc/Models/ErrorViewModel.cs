1fbb934506bca49e2755773ec6db446027cd2e2b
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-05-30 08:55:21 +0700

    setup project

---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyKhoaHoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
