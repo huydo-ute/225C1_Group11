73ba426c47a9412a2cd4dd9a0a33f1b0f60d3211
Hồ Hoàng <hovanhoang959@gmail.com>
2026-05-30 09:15:02 +0700

    HoVanHoang_Create_Enum_Helper

---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Enums/TrangThaiKhoaHoc.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyKhoaHoc.Enums
+{
+    public enum TrangThaiKhoaHoc
+    {
+        [Display(Name = "Sắp mở")]
+        SapMo = 0,
+        [Display(Name = "Đang học")]
+        DangHoc = 1,
+        [Display(Name = "Đã kết thúc")]
+        DaKetThuc = 2
+    }
+}
---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Helpers/LayTenHienThi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace QuanLyKhoaHoc.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string LayTenHienThi(this Enum giaTriEnum)
+        {
+            if (giaTriEnum == null) return string.Empty;
+
+            Type loaiEnum = giaTriEnum.GetType();
+            string tenTruong = giaTriEnum.ToString();
+
+            FieldInfo fieldInfo = loaiEnum.GetField(tenTruong);
+
+            if (fieldInfo == null) return tenTruong;
+            var thuocTinhDisplay = fieldInfo.GetCustomAttribute<DisplayAttribute>();
+            return thuocTinhDisplay?.Name ?? tenTruong;
+        }
+    }
+}
