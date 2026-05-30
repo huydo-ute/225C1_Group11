903ed7398fdba0f3f1882492b66dd5e0f72d5df4
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-05-30 09:46:19 +0700

    QuachNhatPhuong_Migration

---
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyKhoaHoc.Models;
+
+namespace QuanLyKhoaHoc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KhoaHoc> KhoaHocs { get; set; }
+        public DbSet<GiangVien> GiangViens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<KhoaHoc>()
+                .HasOne(k => k.GiangVien)
+                .WithMany(g => g.lstKhoaHocs)
+                .HasForeignKey(k => k.GiangVienId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
